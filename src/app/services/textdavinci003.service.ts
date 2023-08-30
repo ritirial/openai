f72cd56eb99dc1aab1be25f05c8dc64a019b7d69
Ricardo Tinoco <ricardo@nucliux.mx>
2023-08-30 17:09:03 -0600

    key in .env

--- src/app/services/textdavinci003.service.ts
+++ src/app/services/textdavinci003.service.ts
@@ -15,7 +15,7 @@ export class Textdavinci003Service {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer '
+      'Authorization': 'Bearer ' + import.meta.env['NG_APP_KEY'],
     })
   }
 
